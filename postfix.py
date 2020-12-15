113e9876623e85e46cbdb84ebcd752b60d12a673
Tomas-Homola <homola.tom6@gmail.com>
2020-12-15 12:49:25 +0100

    funkcia to_infix zrejme funkcna

--- postfix.py
+++ postfix.py
@@ -1,15 +1,18 @@
 
-
-def eval_expr(s):
+def eval_expr(s, d = {}):
     
     temp = []
     s = s.split() # rozdelenie na list
 
     for item in s:
         
-        if (item.isnumeric()): # ak to je cislo, tak sa prida do temp zoznamu
+        if (item.isnumeric()): # ak je to cislo
             temp.append(item)
-        else: # ak to nie je cislo, tak uz sa predpoklada, ze v temp zozname su uz 2 cisla, tie sa vyberu a daju do "a" a "b"
+
+        elif (item in d): # ak je to premenna, tak potom prida do temp jej hodnotu zo slovnika
+            temp.append(d[item])
+        
+        else: # ak je znamienko, tak pocita
             druhe = int(temp.pop())
             prve = int(temp.pop())
             
@@ -25,3 +28,37 @@ def eval_expr(s):
 
 
     return temp[0]
+
+def to_infix(s):
+
+    temp = []
+    s = s.split()
+
+    for item in s:
+
+        if (item.isnumeric()): # znovu, ak je to cislo, tak ho da do tempu
+            temp.append(item)
+
+        else: # ak je to znamienko
+            druhe = temp.pop()
+            prve = temp.pop()
+
+            if (item == "+"):
+                temp.append('( ' + prve + ' + ' + druhe + ' )')
+            
+            elif (item == "-"):
+                temp.append('( ' + prve + ' + ' + druhe + ' )')
+
+            elif (item == "*"):
+                temp.append('( ' + prve + ' + ' + druhe + ' )')
+
+            elif (item == "/"):
+                temp.append('( ' + prve + ' + ' + druhe + ' )')
+
+    return temp[0]
+
+
+
+
+
+
